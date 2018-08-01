04dcf4efc14ca13812ef9ba6f8a8893216cd7edc
jakemckenzie <jake314@uw.edu>
2018-08-01 09:03:42 -0700

    isqrt

---
+++ isqrt.c
@@ -0,0 +1,13 @@
+//From pages 295-297 of Hacker's Delight by Henry S. Warren Jr
+int isqrt(unsigned x) {
+    unsigned a, b, m;
+    a = 1;
+    b = (x >> 5) + 8;
+    if (b > 65535) b = 65535;
+    do {
+        m = (a + b) >> 1;
+        if (m*m > x) b = m - 1;
+        else         a = m + 1;
+    } while (b >= a);
+    return a - 1;
+}
