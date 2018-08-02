62cb625f92f999e4f890e2967d6cc3d49dbf2b2e
jakemckenzie <jake314@uw.edu>
2018-08-02 16:11:23 -0700

    problem 1 done

---
+++ Week0/GoingFishing.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class GoingFishing{
+    public static void main(String args[]) {
+        System.out.println(fishing(10,5,2,3,0,0,8,0));
+    }
+    public static int fishing(int F1, int F2, int D1, int D2, int i, int j, int k, int sum) { 
+        if (k <= 0) return sum;
+        if (F1 > 0 || F2 > 0) {
+            if (F1 >= F2) {
+                System.out.println(F1 + " " + k+ " " + i + " " + j);
+                return fishing(F1 - D1, F2, D1, D2, ++i, j, --k, sum + F1);
+            } else {
+                System.out.println(F2+ " " + k+ " " + i + " " + j);
+                return fishing(F2 - D2, F1, D2, D1, ++j, i, (k - Math.abs(i - j)), sum + F2);
+            }
+        }
+        if (F1 < 0) return sum + F2;
+        if (F2 < 0) return sum + F1;
+        return sum;    
+    }
+}
+
