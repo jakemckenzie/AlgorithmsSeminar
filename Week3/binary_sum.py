03ee5bbfeb4686644df16a53a0766c46f997004b
jakemckenzie <jake314@uw.edu>
2018-08-05 11:19:46 -0700

    added some code

---
+++ Week3/binary_sum.py
@@ -0,0 +1,11 @@
+def binary_sum(x):
+    if len(x) == 0: return 0;
+    if len(x) == 1: return x[0];
+
+    y = []
+    for i in range(1,len(x),2):
+        y.append(x[i - 1] + x[i])
+    if i + 1 < len(x):
+        y[0] += x[i + 1]
+    
+    return binary_sum(y)
