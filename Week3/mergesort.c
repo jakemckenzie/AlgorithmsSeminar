7b2d49cd09c0f06993e4dfef5b803cd101822220
jakemckenzie <jake314@uw.edu>
2018-08-05 14:41:43 -0700

    added some more stuff

--- Week3/mergesort.c
+++ Week3/mergesort.c
@@ -1,10 +1,7 @@
 void MergeSort(int [] a, int L, int R) {
-
-    if (R <= L) return;
-
-    int mid = (R-L)/2 + L;
-
-    MergeSort(a, L, mid);
-    MergeSort(a, mid+1, R);
-    Merge(a, L, mid, R);
+    if (R <= L) return;         // 1
+    int mid = (R+L)/2;          // 2
+    MergeSort(a, L, mid);       // 3
+    MergeSort(a, mid+1, R);     // 4
+    Merge(a, L, mid, R);        // 5
 }
