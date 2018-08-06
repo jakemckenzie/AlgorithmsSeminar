e70583f09351334c437c03666ca63fbc2baa1f46
jakemckenzie <jake314@uw.edu>
2018-08-06 14:09:10 -0700

    typo fix

--- Week3/binary_sum.py
+++ Week3/binary_sum.py
@@ -3,7 +3,7 @@ def binary_sum(x):
     if len(x) == 1: return x[0];
 
     y = []
-    for i in range(1,len(x),2):#range(state,stop,step)
+    for i in range(1,len(x),2):#range(start,stop,step)
         y.append(x[i - 1] + x[i])
     if i + 1 < len(x):
         y[0] += x[i + 1]
