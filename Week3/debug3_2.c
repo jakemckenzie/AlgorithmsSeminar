519fdb9b788f39388b4cff7ba6d0a40a3f9d0c76
jakemckenzie <jake314@uw.edu>
2018-10-23 14:15:39 -0700

    added debug problem

--- Week3/debug3_2.c
+++ Week3/debug3_2.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
+
 int main(void) {
     int n = 8;
-    while (!foo(n)){
-        switch(n % 2 == 0){
-            case 1:
-            n = n - foo(n - 1);
-            case 0:
-            n = n - foo(n);
-        }
-    } 
+    while (foo(n) != 0){
+        n = n - foo(n);
+    }
+    printf("%d",n);
 }
 
 int foo(int n){
-    int i = 2;
     int sum = 1;
-    while (i * i <= n) {
-        if (n & i == 0) {
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
             sum = sum + i + n / i;
         }
-        i = i + 1;
     }
-    return sum == b && n != 1 ? 0 : 1;
+    if (sum == n && n != 1) {
+        return 0;
+    }
+    return 1;
 }
